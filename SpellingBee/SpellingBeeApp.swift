74fcc42b3bd158d31ad995005dc317266df6263f
William Harrington <wharrington@lcs.on.ca>
2022-02-22 17:17:20 -0500

    Initial Commit

---
+++ SpellingBee/SpellingBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingBeeApp.swift
+//  SpellingBee
+//
+//  Created by William Robert Harrington on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct SpellingBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
